c9e7a13bb3a212bc949424b481423f35b7362543
Dental Academy <dental@academy.com>
2026-05-30 12:45:52 +0300

    Fix sitemap 500 error: handle null updated_at on PostgreSQL

--- server.js
+++ server.js
@@ -245,7 +245,8 @@ app.get('/sitemap.xml', async (req, res, next) => {
     urls.push('<url><loc>' + (process.env.APP_URL || 'https://dental-academy-production.up.railway.app') + '/faq</loc><priority>0.5</priority></url>');
     urls.push('<url><loc>' + (process.env.APP_URL || 'https://dental-academy-production.up.railway.app') + '/contact</loc><priority>0.5</priority></url>');
     courses.forEach(function(c) {
-      urls.push('<url><loc>' + (process.env.APP_URL || 'https://dental-academy-production.up.railway.app') + '/courses/' + c.slug + '</loc><lastmod>' + (c.updated_at || '').substring(0, 10) + '</lastmod><priority>0.7</priority></url>');
+      var lastmod = c.updated_at ? String(c.updated_at).substring(0, 10) : '';
+      urls.push('<url><loc>' + (process.env.APP_URL || 'https://dental-academy-production.up.railway.app') + '/courses/' + c.slug + '</loc>' + (lastmod ? '<lastmod>' + lastmod + '</lastmod>' : '') + '<priority>0.7</priority></url>');
     });
     res.header('Content-Type', 'application/xml');
     res.send('<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' + urls.join('') + '</urlset>');
